fe6ba00714c88fe267e2d9a6246e954aba41c0c4
Kimberly L Palmer <kimberlylpalmer@gmail.com>
2023-09-17 11:28:50 -0700

    lab reconfigured with an arrowFunction

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+// function addingEventListener() {
+//     const input = document.getElementById('button');
+
+// const clickAlertFunction = (clickAlert) => alert('I was clicked!')
+// input.addEventListener('click', clickAlertFunction);
+// }
+// addingEventListener()
+
 function addingEventListener() {
-    const input = document.getElementById('button');
+  const input = document.getElementById('button');
 
-const clickAlertFunction = (clickAlert) => {alert('I was clicked!');}
-
-input.addEventListener('click', clickAlertFunction);
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+addingEventListener();
